cb695094ed7d754f0958072994b8e676197854bf
Tien Le <work.lethanhtien@gmail.com>
2023-12-06 14:25:28 +0700

    feat: add sharing kitchen, getting notification about sharing invitation features, profile fragment, divide kitchen tab layouts, and modifying UI

--- app/src/main/java/com/application/freshfoodapp/adapter/KitchenAdapter.java
+++ app/src/main/java/com/application/freshfoodapp/adapter/KitchenAdapter.java
@@ -1,18 +1,12 @@
 package com.application.freshfoodapp.adapter;
 
-import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.AsyncDifferConfig;
-import androidx.recyclerview.widget.DiffUtil;
-import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.application.freshfoodapp.databinding.ProductItemCardBinding;
@@ -22,11 +16,9 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
